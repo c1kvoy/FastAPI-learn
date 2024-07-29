a39abb3e8d26dbaf9a3c605f2274d158ccd5d576
c1kvoy <seva33039@gmail.com>
2024-07-29 20:15:41 +0700

    sessionmaker now async too

--- async_core/db.py
+++ async_core/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -6,6 +6,6 @@ engine = create_async_engine(
     "sqlite+aiosqlite:///./ProductDB.db"
 )
 
-Session_async = sessionmaker(bind=engine, class_=AsyncSession, autocommit=False, autoflush=False)
+Session_async = async_sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
